2141d6baaa8ca5bf450a654bf10dd6b9b71d0abe
Juanfran <juanfran.alcantara@kaleidos.net>
2018-07-16 09:59:36 +0200

    wip grib

--- Player.gd
+++ Player.gd
@@ -11,17 +11,19 @@ func _ready():
 		get_viewport().hdr = false
 
 func _process(delta):
-	if(is_button_pressed(1)):
+	var rightHand = get_node("RightHand");
+	
+	if(rightHand.is_button_pressed(1)):
 		print("1")
 		
-	if(is_button_pressed(2)):
+	if(rightHand.is_button_pressed(2)):
 		print("2")
 		
-	if(is_button_pressed(3)):
+	if(rightHand.is_button_pressed(3)):
 		print("3")
 		
-	if(is_button_pressed(4)):
+	if(rightHand.is_button_pressed(4)):
 		print("4")
 		
-	if(is_button_pressed(5)):
+	if(rightHand.is_button_pressed(5)):
 		print("5")
