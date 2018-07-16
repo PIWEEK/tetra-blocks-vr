2b083da3beee8fea14510b669723f82292ec2bb9
Juanfran <juanfran.alcantara@kaleidos.net>
2018-07-16 10:04:30 +0200

    wip grip

--- Player.gd
+++ Player.gd
@@ -16,7 +16,8 @@ func _process(delta):
 	if(rightHand.is_button_pressed(1)):
 		print("1")
 		
-	if(rightHand.is_button_pressed(2)):
+	# grid
+	if(rightHand.is_button_pressed(2)): 
 		print("2")
 		
 	if(rightHand.is_button_pressed(3)):
@@ -27,3 +28,19 @@ func _process(delta):
 		
 	if(rightHand.is_button_pressed(5)):
 		print("5")
+		
+	if(rightHand.is_button_pressed(6)):
+		print("6")
+
+	if(rightHand.is_button_pressed(7)):
+		print("7")
+
+	if(rightHand.is_button_pressed(8)):
+		print("8")
+
+	if(rightHand.is_button_pressed(9)):
+		print("9")
+
+	if(rightHand.is_button_pressed(10)):
+		print("10")
+
