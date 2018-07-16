ee087930ea6f8957c93fbf767dd1419ced7f860c
Juanfran <juanfran.alcantara@kaleidos.net>
2018-07-16 07:50:36 +0200

    init vr

---
+++ Player.gd
@@ -0,0 +1,16 @@
+extends ARVROrigin
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	var vr = ARVRServer.find_interface("OpenVR")
+	if(vr and vr.initialize()):
+		get_viewport().arvr = true
+		get_viewport().hdr = false
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
